44fd943957971c4450f4d8110487e5e559a729ab
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-09-14 00:27:13 +0700

    first
    otw Deploy

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Public Speaking</li>
+        <li>Public Relation</li>
+        <li>Digital Marketing</li>
+        <p className="text-center">Programming Language</p> 
+        <li>Python</li>
+        <li>HTML & CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MAN Insan Cendekia Serpong</li>
+        <li>Institut Teknologi Bandung</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>DQ Lab - Data Science</li>
+        <li>RevoU - Software Engineering</li>
+        <p>MySkill</p>
+        <li>Data Analysis</li>
+        <li>Digital Marketing</li>
+        <li>Software Engineering</li>
+        <li>Product Management</li>
+        <li>Finance</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          My Name is Ghazy Achmed Movlech Urbayani. I like to be called Ghazy. I am 
+          passionate about Public Speaking and Communication. Furthermore, computers and technology has been my interest since 
+          childhood. Due to that, a computer science or system information major has piqued my interest in the present and future.
+          I am a Junior Web Developer with a passion for creating
+          interactive and responsive web applications. I have experience
+          working with JavaScript, React, HTML, CSS, and Git. I am a quick learner and I am always
+          looking to expand my knowledge and skill set. I am a team player and
+          I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
