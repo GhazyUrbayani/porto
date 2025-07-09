bb2befd6b6e00b744bc78ba20d1a85887f4db52b
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2025-07-09 10:33:06 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   title: "Ghazy's Porto",
     description: 'Personal Portfolio showcasing skills, projects, and achievements.',
   icons: {
-    icon: '/images/hero-image.png',
+    icon: 'favicon.ico',
   },
 }
 
