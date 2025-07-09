9528ee8e15baa84777edcc0a9203f3d1c8e31122
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2025-07-09 10:34:58 +0700

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           href={"/#home"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          Ghazy's Porto
+          Ghazy&apos;s Porto
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
