4c515d1f5a139471171bbe5bb2afebd1aa0f91a1
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2025-07-09 12:27:22 +0700

    Update AchievementsSection.jsx

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -24,12 +24,13 @@ const achievementsList = [
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-around"> {/* Perubahan di sini: justify-around atau justify-evenly untuk distribusi lebih merata */}
         {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+              // Hapus mx-4 jika ingin lebih rapat atau sesuaikan
+              className="flex flex-col items-center justify-center my-4 sm:my-0 sm:mx-8" /* Tambahkan sm:mx-8 atau sesuaikan untuk jarak horizontal di layar besar */
             >
               <h2 className="text-white text-4xl font-bold flex flex-row">
                 {achievement.prefix}
